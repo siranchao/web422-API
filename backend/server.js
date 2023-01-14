d067705de2848a2c386bb7737f1255bf4c60ced6
Siran Cao <siranchao@gmail.com>
2023-01-14 01:11:25 -0500

    submission

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Siran Cao   |   Student ID: 159235209    |     Date: Jan 14 2023
+*  Cyclic Link: https://misty-mite-purse.cyclic.app/
+*
+********************************************************************************/
+
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
